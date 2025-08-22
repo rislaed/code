8344682fae3f2c4ef83f3a8d361c4b8e94dc7e12
Alejandro González <me@alegon.dev>
2025-08-22 23:55:42 +0200

    chore: expose file IDs in version queries

--- apps/labrinth/src/routes/v3/version_creation.rs
+++ apps/labrinth/src/routes/v3/version_creation.rs
@@ -446,6 +446,7 @@ async fn version_create_inner(
             .files
             .iter()
             .map(|file| VersionFile {
+                id: None,
                 hashes: file
                     .hashes
                     .iter()
